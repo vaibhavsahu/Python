bb4b66bc97a573d3be175a5e0b125e255ea2103b
vaibhav sahu <vaibh3jec@gmail.com>
2018-07-01 22:15:44 -0600

    print directory contents without using os.walk

---
+++ print dir contents/printDir.py
@@ -0,0 +1,18 @@
+import os
+def print_directory_contents(sPath):
+    """
+    This function takes the name of a directory 
+    and prints out the paths files within that 
+    directory as well as any files contained in 
+    contained directories. 
+
+    This function is similar to os.walk. Please don't
+    use os.walk in your answer. We are interested in your 
+    ability to work with nested structures. 
+    """
+    for sChild in os.listdir(sPath):                
+        sChildPath = os.path.join(sPath,sChild)
+        if os.path.isdir(sChildPath):
+            print_directory_contents(sChildPath)
+        else:
+            print(sChildPath)
